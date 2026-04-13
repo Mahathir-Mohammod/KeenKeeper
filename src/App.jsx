fc225a4891f8a62c871b4cf99087b0b7d229b76d
Mahathir-Mohammod <mahathirmohammod88@gmail.com>
2026-04-14 02:30:39 +0600

    fix the footer bug. it's shows 2time at the same time one under another

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function Home() {
     <>
       <Banner />
       <Friends />
-      <Footer />
     </>
   )
 }
@@ -20,7 +19,6 @@ function FriendsPage() {
   return (
     <>
       <Friends />
-      <Footer />
     </>
   )
 }
