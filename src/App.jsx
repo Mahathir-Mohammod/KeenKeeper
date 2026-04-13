1be423814ed7ded235621b9e898fa667d82140c1
Mahathir-Mohammod <mahathirmohammod88@gmail.com>
2026-04-14 01:38:37 +0600

    make the friends grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Banner from './Components/Banner/Banner'
 import Footer from './Components/Footer/Footer'
+import Friends from './Components/Friends/Friend'
 
-function App() {
-
+function Home() {
   return (
     <>
-    <Navbar></Navbar>
-    <Banner></Banner>
-    <Footer></Footer>
+      <Banner />
+      <Friends />
+      <Footer />
     </>
+  )
+}
+
+function FriendsPage() {
+  return (
+    <>
+      <Friends />
+      <Footer />
+    </>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/friends" element={<FriendsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
